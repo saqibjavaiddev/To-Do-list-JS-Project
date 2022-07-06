36d95966de6c3bdf94b7178a0c60c46a29499099
msaqibjavaid <duckyprogrammer@gmail.com>
2022-07-06 14:03:21 +0500

    Fixed JS Linters

--- src/displayhtml.js
+++ src/displayhtml.js
@@ -1,17 +1,17 @@
-import localstore from './localstore.js'; 
+import localstore from './localstore.js';
 
-export default (() =>  { 
-  const addListItem = (item) => { 
-    const newItem = document.createElement('li'); 
-    newItem.classList.add('todoItem'); 
-    const newIcon = document.createElement('i'); 
-    newIcon.classList.add('fa', 'fa-ellipsis-v'); 
-    const newIconTrash = document.createElement('i'); 
-    newIconTrash.classList.add('fa', 'fa-trash', 'hideellipsis'); 
-    newIconTrash.setAttribute('data-index', item.index); 
-    const checkbox = document.createElement('input'); 
-    checkbox.type = 'checkbox'; 
-    checkbox.checked = item.completed; 
+export default (() => {
+  const addListItem = (item) => {
+    const newItem = document.createElement('li');
+    newItem.classList.add('todoItem');
+    const newIcon = document.createElement('i');
+    newIcon.classList.add('fa', 'fa-ellipsis-v');
+    const newIconTrash = document.createElement('i');
+    newIconTrash.classList.add('fa', 'fa-trash', 'hideellipsis');
+    newIconTrash.setAttribute('data-index', item.index);
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = item.completed;
     checkbox.id = item.index;
     checkbox.classList.add('checkbox');
     const inputDesc = document.createElement('input');
@@ -28,16 +28,16 @@ export default (() =>  {
     const list = document.querySelector('#todo-list');
     list.insertBefore(newItem, btn);
   };
-// Display List
-  const displayList = () =>  { 
+  // Display List
+  const displayList = () => {
     const LocalStoragelist = localstore.gettasks();
-    LocalStoragelist.forEach((item)  =>  { 
+    LocalStoragelist.forEach((item) => {
       addListItem(item);
-    }); 
-  }; 
-
-  return { 
-    displayList, 
-    addListItem, 
+    });
   };
-})(); 
+
+  return {
+    displayList,
+    addListItem,
+  };
+})();
--- src/index.js
+++ src/index.js
@@ -1,64 +1,63 @@
-import './style.css'; 
-import localstore from  './localstore.js'; 
-import displayhtml from  './displayhtml.js'; 
+import './style.css';
+import localstore from './localstore.js';
+import displayhtml from './displayhtml.js';
 
+const todoList = [];
 
-const todoList = []; 
-
-const renderUI = ()  =>  { 
-  if (!JSON.parse(localStorage.getItem('todo-list')))  { 
-    localstore.storetask(localstore.sortindex(todoList)); 
-  } 
-// Display List of tasks in UI
-  displayhtml.displayList(); 
-// Event for Checkboxes
-  const checkboxes = document.querySelectorAll('.checkbox'); 
-  [...checkboxes].forEach((button) =>  { 
-    button.addEventListener('change', localstore.completeTask); 
-  }); 
-// Event for Update Task
-  const inputs = document.querySelectorAll('.description'); 
-  [...inputs].forEach((input) =>  { 
-    input.addEventListener('focusout', localstore.updateTask); 
-  }); 
-// Hide Ellipsis icon and display trash icon 
-  [...inputs].forEach((input) =>  { 
+const renderUI = () => {
+  if (!JSON.parse(localStorage.getItem('todo-list'))) {
+    localstore.storetask(localstore.sortindex(todoList));
+  }
+  // Display List of tasks in UI
+  displayhtml.displayList();
+  // Event for Checkboxes
+  const checkboxes = document.querySelectorAll('.checkbox');
+  [...checkboxes].forEach((button) => {
+    button.addEventListener('change', localstore.completeTask);
+  });
+  // Event for Update Task
+  const inputs = document.querySelectorAll('.description');
+  [...inputs].forEach((input) => {
+    input.addEventListener('focusout', localstore.updateTask);
+  });
+  // Hide Ellipsis icon and display trash icon
+  [...inputs].forEach((input) => {
     input.addEventListener('focus', (event) => {
-      event.target.style.backgroundColor = '#fff4bf'; 
-      event.target.parentElement.style.backgroundColor = '#fff4bf'; 
-      event.target.nextSibling.classList.add('hideellipsis'); 
-      event.target.nextSibling.nextSibling.classList.remove('hideellipsis'); 
-    }); 
-  }); 
-// Trash task from list
-  const trashes =  document.querySelectorAll('.fa-trash'); 
-  [...trashes].forEach((trash) =>  {  
-    trash.addEventListener('click', (event) =>   {  
-      localstore.remove(event); 
-      const oldList = document.querySelectorAll('.todoItem');  
-      [...oldList].forEach((e) => e.remove());  
-      renderUI();  
-    }); 
-  }); 
-}; 
+      event.target.style.backgroundColor = '#fff4bf';
+      event.target.parentElement.style.backgroundColor = '#fff4bf';
+      event.target.nextSibling.classList.add('hideellipsis');
+      event.target.nextSibling.nextSibling.classList.remove('hideellipsis');
+    });
+  });
+  // Trash task from list
+  const trashes = document.querySelectorAll('.fa-trash');
+  [...trashes].forEach((trash) => {
+    trash.addEventListener('click', (event) => {
+      localstore.remove(event);
+      const oldList = document.querySelectorAll('.todoItem');
+      [...oldList].forEach((e) => e.remove());
+      renderUI();
+    });
+  });
+};
 // Add Item in the list when user press enter after input
-const input = document.querySelector('#additem'); 
-input.addEventListener('keyup', (event) =>  { 
+const input = document.querySelector('#additem');
+input.addEventListener('keyup', (event) => {
   if (event.keyCode === 13) { // Returns Enter Key
-    localstore.add(event);  
-    event.target.value = '';  
-    const oldList = document.querySelectorAll('.todoItem'); 
-    [...oldList].forEach((e) =>  e.remove()); 
-    renderUI(); 
-  } 
-}); 
-const btn = document.querySelector('.btn'); 
-btn.addEventListener('click', (e)  =>  { 
-  e.preventDefault(); 
-  localstore.clearCompleted(); 
-  const oldList = document.querySelectorAll('.todoItem'); 
-  [...oldList].forEach((e) => e.remove()); 
-  renderUI(); 
-}); 
+    localstore.add(event);
+    event.target.value = '';
+    const oldList = document.querySelectorAll('.todoItem');
+    [...oldList].forEach((e) => e.remove());
+    renderUI();
+  }
+});
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  localstore.clearCompleted();
+  const oldList = document.querySelectorAll('.todoItem');
+  [...oldList].forEach((e) => e.remove());
+  renderUI();
+});
 
-renderUI();  
+renderUI();
