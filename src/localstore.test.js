75c86fa1e803074561dee6560c4269ba4517197d
nedith <nedith22@gmail.com>
2022-07-13 15:19:46 +0300

    Add test for remove method

--- src/localstore.test.js
+++ src/localstore.test.js
@@ -24,21 +24,21 @@ describe('Add Method', () => {
   });
 });
 
-// describe('Remove Method', () => {
-//   test('Remove from the list', () => {
-//     expect(localstore.remove({ target: { index: '1' } })).toBe(2);
-//   });
+describe('Remove Method', () => {
+  test('Remove from the list', () => {
+    expect(localstore.remove({ target: { index: '1' } })).toBe(2);
+  });
 
-//   test('Remove one new item from the list', () => {
-//     document.body.innerHTML = `<ul id="todo-list">
-//     <li class="head">Today's To Do<i class="fa fa-refresh" aria-hidden="true"></i></li>
-//     <li class="add-item"><input type="text" id="additem" placeholder="Add to your list ..."></li>
-//     <li class="btn"><a href="#">Clear all Completed</a></li>
-//     </ul>`;
+  test('Remove one new item from the list', () => {
+    document.body.innerHTML = `<ul id="todo-list">
+    <li class="head">Today's To Do<i class="fa fa-refresh" aria-hidden="true"></i></li>
+    <li class="add-item"><input type="text" id="additem" placeholder="Add to your list ..."></li>
+    <li class="btn"><a href="#">Clear all Completed</a></li>
+    </ul>`;
 
-//     localstore.displayList();
-//     localstore.remove({ target: { index: '1' } });
-//     const list = document.querySelectorAll('.todoItem');
-//     expect(list).toHaveLength(1);
-//   });
-// });
+    localstore.displayList();
+    localstore.remove({ target: { index: '1' } });
+    const list = document.querySelectorAll('.todoItem');
+    expect(list).toHaveLength(1);
+  });
+});
